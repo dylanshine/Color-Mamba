7b0c5bfa11bc4e72a092def5bfd986dc968a85e6
Dylan Shine <dylanshinenyc@gmail.com>
2015-06-11 15:09:06 -0400

    Add initWithSize to scene

--- Colorpillar/GameScene.m
+++ Colorpillar/GameScene.m
@@ -19,5 +19,28 @@
     SKLabelNode *_scoreBoard;
 }
 
+-(instancetype)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        _colors = @[[UIColor redColor], [UIColor blueColor], [UIColor greenColor]];
+        _directions = @{@"up": [SKAction moveBy:CGVectorMake(0, 3) duration:0.3],
+                        @"down": [SKAction moveBy:CGVectorMake(0, -3) duration:0.3],
+                        @"left": [SKAction moveBy:CGVectorMake(-3, 0) duration:0.3],
+                        @"right": [SKAction moveBy:CGVectorMake(3, 0) duration:0.3]};
+        _lives = 3;
+        _bodyNodes = [[NSMutableArray alloc]init];
+        _currentDirection = @"up";
+        _colorpillar = [SKSpriteNode spriteNodeWithColor:[UIColor blackColor] size:CGSizeMake(20, 20)];
+        _colorpillar.position = CGPointMake(self.size.width/2, self.size.height/2);
+        _colorpillar.zPosition = 100;
+         self.backgroundColor = [SKColor whiteColor];
+        [self addChild:_colorpillar];
+        
+    }
+    return self;
+}
+
 
 @end
+
+
+
