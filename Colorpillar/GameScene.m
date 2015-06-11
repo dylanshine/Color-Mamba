0d164d3ae207eb7babac3bd02d8a3083b27d7e85
Dylan Shine <dylanshinenyc@gmail.com>
2015-06-11 14:36:55 -0400

    Add private variables

--- Colorpillar/GameScene.m
+++ Colorpillar/GameScene.m
@@ -8,42 +8,16 @@
 
 #import "GameScene.h"
 
-@implementation GameScene
-
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
+@implementation GameScene{
+    SKSpriteNode *_colorpillar;
+    NSString *_currentDirection;
+    NSDictionary *_directions;
+    NSMutableArray *_bodyNodes;
+    UIColor *_currentColor;
+    NSArray *_colors;
+    NSUInteger _lives;
+    SKLabelNode *_scoreBoard;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
-}
-
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
-}
 
 @end
