630bc77e29f85ce7a52724676bbf606d7451e756
Dylan Shine <dylanshinenyc@gmail.com>
2015-06-11 11:22:57 -0400

    Initial Commit

---
+++ Colorpillar/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Colorpillar
+//
+
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Colorpillar/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Colorpillar
+//
+
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
